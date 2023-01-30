60b3197737bd0b116474ec160c41c3d95dcfbb98
Clarkok Zhang <mail@clarkok.com>
2023-01-30 16:05:36 +0800

    [nvim] Fix LSP buffer keymap

--- nvim/init.lua
+++ nvim/init.lua
@@ -140,11 +140,22 @@ require('lazy').setup({
     },
 
     -- Treesitter
+    { 'nvim-treesitter/nvim-treesitter-textobjects', build = ':TSUpdate', lazy = true },
+    {
+        'nvim-treesitter/nvim-treesitter-context',
+        lazy = true,
+        opts = {
+            enable = true,
+            max_lines = 5,
+            pattern = { cpp = { 'lambda_expression' } }
+        }
+    },
     {
         'nvim-treesitter/nvim-treesitter',
         ft = code_file_types,
         build = ':TSUpdate',
         lazy = true,
+        dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects', 'nvim-treesitter/nvim-treesitter-context' },
         config = function()
             require('nvim-treesitter.install').compilers = { 'clang', 'gcc' }
 
@@ -196,17 +207,7 @@ require('lazy').setup({
             }
         end
     },
-    { 'nvim-treesitter/nvim-treesitter-textobjects', build = ':TSUpdate', lazy = true, dependencies = { 'nvim-treesitter/nvim-treesitter' } },
-    {
-        'nvim-treesitter/nvim-treesitter-context',
-        lazy = true,
-        opts = {
-            enable = true,
-            max_lines = 5,
-            pattern = { cpp = { 'lambda_expression' } }
-        }
-    },
-    { 'nvim-treesitter/playground', lazy = true, cmd = 'TSPlaygroundToggle' },
+    { 'nvim-treesitter/playground', lazy = true, cmd = 'TSPlaygroundToggle', dependencies = { 'nvim-treesitter/nvim-treesitter' } },
 
     -- File Management
     {
@@ -297,28 +298,35 @@ require('lazy').setup({
 
             -- keybindings
             local on_attach = function(client, bufnr)
-                local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-                local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+                local opts = { noremap=true, silent=true, buffer=bufnr }
 
-                -- buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-                local opts = { noremap=true, silent=true }
-
-                buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-                buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-                buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-                buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-                buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-                buf_set_keymap('n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-                buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-                buf_set_keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-                buf_set_keymap('n', '<leader>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-                buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-                buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-                buf_set_keymap('n', '<leader>=', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+                vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+                vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+                vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+                vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+                vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+                vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+                vim.keymap.set('n', '<leader>=', vim.lsp.buf.formatting, opts)
 
                 require "lsp_signature".on_attach({ bind = true }, bufnr)
+
+                print("LSP attached ", bufnr)
             end
+
+            nvim_lsp.tsserver.setup {
+                on_attach = on_attach,
+                capabilities = capabilities,
+            }
+
+            nvim_lsp.rust_analyzer.setup {
+                on_attach = on_attach,
+                capabilities = capabilities,
+            }
+
+            nvim_lsp.sumneko_lua.setup {
+                on_attach = on_attach,
+                capabilities = capabilities,
+            }
 
             -- clangd section
             local compile_commands_dir;
@@ -331,24 +339,9 @@ require('lazy').setup({
             end
 
             nvim_lsp.clangd.setup {
-                on_attach,
+                on_attach = on_attach,
+                capabilities = capabilities,
                 cmd = { 'C:\\Program Files\\LLVM\\bin\\clangd.exe', '--pch-storage=memory', compile_commands_dir, '--background-index' },
-                capabilities
-            }
-
-            nvim_lsp.tsserver.setup {
-                on_attach,
-                capabilities
-            }
-
-            nvim_lsp.rust_analyzer.setup {
-                on_attach,
-                capabilities,
-            }
-
-            nvim_lsp.sumneko_lua.setup {
-                on_attach,
-                capabilities,
             }
 
             Autocmd('BufWritePre', {
